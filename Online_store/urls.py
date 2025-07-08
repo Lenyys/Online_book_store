b7746de7365303d392982223bab9e29d61439aa7
Lenka Marešová <lenkamaresu@gmail.com>
2025-07-08 20:08:06 +0200

    ProductListView, product_list.html - without base.html

--- Online_store/urls.py
+++ Online_store/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from eshop.views import ProductListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('eshop/products/', ProductListView.as_view(), name='products'),
+
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from eshop.models import Product
+
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+    context_object_name = 'products'
+
+    paginate_by = 10
+    ordering = ['-price']
+
