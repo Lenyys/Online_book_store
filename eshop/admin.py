fba866b0d4a40782536fe6f92f73f62f266da9f9
Lenka Marešová <lenkamaresu@gmail.com>
2025-07-08 19:29:23 +0200

    admin.py registration models

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from eshop.models import Category, Product, SelectedProduct, Image, Order, Cart
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(SelectedProduct)
+admin.site.register(Image)
+admin.site.register(Cart)
+admin.site.register(Order)
