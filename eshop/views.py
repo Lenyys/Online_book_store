d646da137655fc95234d868ff6a0a07739bb1523
Veronika <petr.markvart.1983@gmail.com>
2025-07-10 14:58:37 +0200

    přidání editace a mazání kategorií

--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'eshop/category_list.html', {'categories': categories})
+
+
+from django.shortcuts import get_object_or_404, redirect
+from .forms import CategoryForm
+
+def category_edit(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category-list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'eshop/category_form.html', {'form': form})
+
+
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["GET", "POST"])
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category-list')
+    return render(request, 'eshop/category_confirm_delete.html', {'category': category})
