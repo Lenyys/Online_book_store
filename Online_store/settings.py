0eadc45777a994ff22e154b17166c61ccebc4968
Lenka Marešová <lenkamaresu@gmail.com>
2025-08-11 06:03:38 +0200

    language_cs

--- Online_store/settings.py
+++ Online_store/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
 TIME_ZONE = 'UTC'
 
